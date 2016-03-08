7f4ccc2753615f6407ca463c91f49b60c1c09a5a
kmaheshwari <maheshwari.kajol@gmail.com>
2016-03-08 17:11:24 +0530

    Login complete

--- app/assets/javascripts/landing/static_pages.js
+++ app/assets/javascripts/landing/static_pages.js
@@ -39,6 +39,7 @@ function statusChangeCallback(response) {
     if (response.status === 'connected') {
       // Logged into your app and Facebook.
       testAPI();
+      window.location = '/dashboard';
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
       document.getElementById('status').innerHTML = 'Please log ' +
