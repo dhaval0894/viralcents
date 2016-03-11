0747b2d66affaba6a41ce28af997005733adb6ae
rajiv-shrivastava <rjv834@gmail.com>
2016-03-11 18:25:59 +0530

    live chat added

--- app/assets/javascripts/user_panel.js
+++ app/assets/javascripts/user_panel.js
@@ -1,13 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function() {
-	var clip = new ZeroClipboard($("#d_clip_button"))
-	
-	//menu-toggle
-	$("#menu-toggle").click(function(e) {
-    	e.preventDefault();
-    	$("#wrapper").toggleClass("toggled");
-	});
+	var clip = new ZeroClipboard($("#d_clip_button"))	
+});
+
+//menu-toggle
+$("#menu-toggle").click(function(e) {
+	e.preventDefault();
+	$("#wrapper").toggleClass("toggled");
 });
 
 
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+$.src="//v2.zopim.com/?3kxTxB6zH9i31AZ0o3sfl0yiRkWHCToF";z.t=+new Date;$.
+type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
