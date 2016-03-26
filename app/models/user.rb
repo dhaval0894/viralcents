f5ba0ae39aed311b4cb5b7368d99389f4e1afda7
kmaheshwari <maheshwari.kajol@gmail.com>
2016-03-26 17:08:05 +0530

    fb likes count added

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,52 @@ class User < ActiveRecord::Base
 	    user.save!
 	  end
 	end
+	def facebook
+		@facebook ||= Koala::Facebook::API.new(oauth_token)
+	end
+
+	def fb_likes(post_id)
+		# facebook.get_object('966978620053068_971364312947832', :fields => "likes.summary(true)")["likes"]["summary"]["total_count"]
+		#full post id for user
+		fpost_id = ""
+		fpost_id = uid + "_"  + post_id
+		begin 
+			facebook.get_object(fpost_id, :fields => "likes.summary(true)")["likes"]["summary"]["total_count"]
+		rescue 
+			return false
+		end
+		
+	end
+
+	def fb_likes_data(post_id)
+		fpost_id = ""
+		fpost_id = uid + "_"  + post_id
+		begin 
+			facebook.get_object(fpost_id, :fields => "likes.summary(true)")
+		rescue 
+			return false
+		end
+		
+	end
+
+	def fb_shares(post_id)
+		fspost_id = ""
+		fspost_id = uid + "_" + post_id
+		begin
+			facebook.get_object('/'+fspost_id+'/sharedposts?limit=10000&format=json').size
+		rescue
+			return false
+		end
+	end 
+
+	def fb_comments(post_id)
+		fcpost_id = ""
+		fcpost_id = uid + "_" + post_id
+		begin
+			facebook.get_object(fcpost_id, :fields => "comments.summary(true)")["comments"]["summary"]["total_count"]
+		rescue
+			return false
+		end
+	end
 
 end
