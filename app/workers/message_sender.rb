049ffb7fba0736109ac4d45ad45e0c3579e9cc92
rajiv-shrivastava <rjv834@gmail.com>
2016-04-02 15:06:32 +0530

    databse.yml changed

--- app/workers/message_sender.rb
+++ app/workers/message_sender.rb
@@ -8,12 +8,12 @@ class MessageSender
 	def perform()
 				require 'net/http'
 				uri = URI('http://sms.hspsms.com:/sendSMS')
-				params = {:username => "nitesh.mishra143@gmail.com", 
+				params = {:username => "secret", 
 					      :message => "the day was cool",
 					      :sendername =>"VIRALS",
 					      :smstype =>"TRANS",
-					      :numbers => "8349320184",
-					      :apikey =>"eaf93dbe-bf68-43c7-8e47-c7a38bae0371"}
+					      :numbers => "number",
+					      :apikey =>"secret"}
 				uri.query = URI.encode_www_form(params)
 				res = Net::HTTP.get_response(uri)
 				puts res.body if res.is_a?(Net::HTTPSuccess)
