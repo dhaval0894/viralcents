543ae513fb1d0e1a54205a40ceef700f16314a6e
kmaheshwari <maheshwari.kajol@gmail.com>
2016-04-06 20:11:41 +0530

    create story improved

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
-//= require jquery.ui.datepicker
 //= require jquery.dotdotdot
 //= require bootstrap-sprockets
 //= require bootbox
