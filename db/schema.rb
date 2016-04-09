4e0f174f294695bba85ddf124c6b1af34b629365
kmaheshwari <a.thakur73.at@gmail.com>
2016-04-09 15:09:01 +0530

    col added for story_status

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160402091800) do
+ActiveRecord::Schema.define(version: 20160409090719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,8 +64,8 @@ ActiveRecord::Schema.define(version: 20160402091800) do
   create_table "stories", force: :cascade do |t|
     t.string   "title"
     t.string   "orig_url"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.string   "image_url"
     t.float    "click_amt",     default: 0.0
     t.float    "like_amt",      default: 0.0
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20160402091800) do
     t.float    "fav_amt",       default: 0.0
     t.float    "retweet_amt",   default: 0.0
     t.integer  "admin_user_id"
+    t.string   "story_status",  default: "active"
   end
 
   create_table "transactions", force: :cascade do |t|
