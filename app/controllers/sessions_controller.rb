fbae5636e4d9f8c7298614798fd9a7e728b42d47
rajiv-shrivastava <rjv834@gmail.com>
2016-04-11 16:40:14 +0530

    sessions improved

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,20 +9,19 @@ class SessionsController < ApplicationController
         url = request.referrer
         uri = URI.parse(url) rescue nil
         ref = CGI.parse(uri.query) rescue nil
-        if ref !=nil and user.referrer.nil? and user.uid != ref["ref"].join(","$
+        if ref !=nil and user.referrer.nil? and user.uid != ref["ref"].join(",")[5..-6]
           @u = User.find(user.id)
           @u.update(referrer: ref["ref"].join(",")[5..-6])
         end      
-  	else
+    else
         @twitter_user = TwitterUser.find_or_create_from_auth_hash(auth_hash)
-  		  session[:tuser_id]=@twitter_user.id
-  	end
+        session[:tuser_id]=@twitter_user.id
+    end
     redirect_to '/dashboard'
   end
 
   def destroy
     #destroy all session variables on logout
-    session[:tuser_id] = nil
     session[:user_id] = nil
     redirect_to root_url
     # redirect_to '/dashboard'
@@ -31,6 +30,6 @@ class SessionsController < ApplicationController
   protected
 
   def auth_hash
-  	request.env['omniauth.auth']
+    request.env['omniauth.auth']
   end
 end
