b4327741dade3706a517fe98fc4b220d343d5e5c
kmaheshwari <a.thakur73.at@gmail.com>
2016-04-12 15:06:52 +0530

    user sign uponly once

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,15 +9,16 @@ class SessionsController < ApplicationController
         url = request.referrer
         uri = URI.parse(url) rescue nil
         ref = CGI.parse(uri.query) rescue nil
-        if ref !=nil and user.referrer.nil? and user.uid != ref["ref"].join(",")[5..-6]
+        @all_uid=User.pluck(:uid)
+        if ref !=nil and user.referrer.nil? and user.uid != ref["ref"].join(",")[5..-6] and !@all_uid.include? user.uid
           @u = User.find(user.id)
           @u.update(referrer: ref["ref"].join(",")[5..-6])
           #update wallet amount for refferer
-          @referer=User.find(uid: ref["ref"].join(",")[5..-6 )
-          @wallet=Wallet.find(user_id: @referer.id)
-          @wallet.update(balance: @wallet.balance+5.0)
-          @new_trans=UserTransaction.new(user_id: @referer.id,amt: @wallet.balance+5.0,trans_type: 'referral',trans_date: DateTime.now)
-          @new_trans.save
+          # @referer=User.find_by(uid: ref["ref"].join(",")[5..-6 ])
+          # @wallet=Wallet.find(user_id: @referer.id)
+          # @wallet.update(balance: @wallet.balance+5.0)
+          # @new_trans=UserTransaction.new(user_id: @referer.id,amt: @wallet.balance+5.0,trans_type: 'referral',trans_date: DateTime.now)
+          # @new_trans.save
         end      
     else
         @twitter_user = TwitterUser.find_or_create_from_auth_hash(auth_hash)
