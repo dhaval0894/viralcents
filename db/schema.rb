de79e88c61b4c673732f6f188b5a4a3ae845ebb3
kmaheshwari <a.thakur73.at@gmail.com>
2016-04-13 15:57:34 +0530

    correct recharge path

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160411173400) do
+ActiveRecord::Schema.define(version: 20160411172523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,11 +99,11 @@ ActiveRecord::Schema.define(version: 20160411173400) do
     t.float    "fav_amt",       default: 0.0
     t.float    "retweet_amt",   default: 0.0
     t.integer  "admin_user_id"
+    t.string   "story_status",  default: "active"
     t.float    "total_budget"
     t.boolean  "published",     default: false
     t.datetime "expiry_date"
     t.integer  "category_id"
-    t.string   "story_status",  default: "active"
   end
 
   create_table "transactions", force: :cascade do |t|
