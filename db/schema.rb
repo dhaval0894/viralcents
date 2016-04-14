85f3c472304735e6add787416559bf7acb8639a2
kmaheshwari <a.thakur73.at@gmail.com>
2016-04-14 15:26:00 +0530

    add col to coupons

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411172523) do
+ActiveRecord::Schema.define(version: 20160414095302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 20160411172523) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.boolean  "coupon_reuse",        default: false
+    t.string   "coupon_code"
+    t.date     "expiry_date"
   end
 
   create_table "mapcoupons", force: :cascade do |t|
