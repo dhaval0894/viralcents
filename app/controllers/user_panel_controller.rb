5c78a973721d9fadbb265b6a09c23ac423247d03
kmaheshwari <a.thakur73.at@gmail.com>
2016-04-18 14:57:43 +0530

    update last week earning

--- app/controllers/user_panel_controller.rb
+++ app/controllers/user_panel_controller.rb
@@ -243,7 +243,8 @@ class UserPanelController < ApplicationController
 		@usr_wallet=Wallet.find_by(user_id: current_user.id)
 		@shared_story = UserStory.where(user_id: current_user.id).count
 		#find week earning
-		@last_week=UserTransaction.where(user_id: current_user.id,trans_type: "credit")
+		@credit_type=["referral","credit"]
+		@last_week=UserTransaction.where(user_id: current_user.id,trans_type: @credit_type)
 		@week_earning=0.0
 		@last_week.each do |l|
 			if l.trans_date > 1.week.ago
