4eb43e5a870879e8180371a5b05e43b3f70ad63b
aditi73 <a.thakur73.at@gmail.com>
2016-04-26 01:13:32 +0530

    clicks analytics scheduler

--- app/workers/social_analytics.rb
+++ app/workers/social_analytics.rb
@@ -16,16 +16,17 @@ class SocialAnalytics
     def perform
     	#to encode url
     	require 'uri'
-    	@current_user = $current_user
+    	@current_user = User.find(1)
      	twitter_user = TwitterUser.find_by(user_id: 1)
    		@my_story = UserStory.where(user_id: 1)
 
 	 	@my_story.each do |ms|
 	 		#add all stories to the list
 		 		
-			# if not ms.fb_post_id.nil?
-			# 	ms.update(fb_likes: @current_user.fb_likes(ms.fb_post_id), fb_shares: @current_user.fb_shares(ms.fb_post_id), fb_comments: @current_user.fb_comments(ms.fb_post_id) )
-			# end
+			if not ms.fb_post_id.nil?
+				ms.update(fb_likes: @current_user.fb_likes(ms.fb_post_id), fb_shares: @current_user.fb_shares(ms.fb_post_id), fb_comments: @current_user.fb_comments(ms.fb_post_id) )
+			end
+			#twitter analytics
 	 		if twitter_user and not ms.tw_post_id.nil?
 	 			#to check whether tweet exist or not
 	 			
@@ -38,15 +39,22 @@ class SocialAnalytics
 	 				@tweet_info=t_user.twitter.status(ms.tw_post_id)
 	 				ms.update(fav: @tweet_info.favorite_count,retweets: @tweet_info.retweet_count)
 	 			end
-
-	# 			
+			end
+			#clicks analytics
+			if not ms.short_url.nil?
+				extra = {
+					:utm_source => 'NOTSET',
+		  			:utm_medium => 'SOCIAL',
+		  			:utm_term => @current_user.uid
+		  
+				}
+				@story=Story.find(ms.story_id)
+				@story_url=[@story.orig_url,'?', extra.to_query].join("")
+				client = Bitly.client
+		      	@url=client.shorten(@story_url)
+				ms.update(clicks: @url.stats["clicks"])
 				
-	 		end
-	# 		# if !ms.short_url.nil?
-	# 		# 	@url=bitly_hash(ms.story_id)
-	# 		# 	ms.update(clicks: @url.stats["clicks"])
-
-	# 		# end
+			end
 	 	end
 	 end
 end
